30358841a72f02fce188622ae8d415d59334f528
Lutfykumar <lutfykumar@users.noreply.github.com>
2021-11-12 10:20:47 +0700

    first

---
+++ index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Portal KOKOLA GROUP">
+  <meta name="author" content="Bangchan">
+  <title>Landing - KOKOLA GROUP</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <style>
+    .bd-placeholder-img {
+      font-size: 1.125rem;
+      text-anchor: middle;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      user-select: none;
+    }
+
+    @media (min-width: 768px) {
+      .bd-placeholder-img-lg {
+        font-size: 3.5rem;
+      }
+    }
+  </style>
+  <link href="assets/css/heroes.css" rel="stylesheet">
+</head>
+
+<body>
+  <main>
+    <h1 class="visually-hidden">Heroes examples</h1>
+
+    <div class="px-4 py-5 my-5 text-center">
+      <img class="d-block mx-auto w-auto mb-4" src="assets/img/kokola.png" alt="" width="72" height="57">
+      <h1 class="display-5 fw-bold">Portal KOKOLA GROUP</h1>
+      <div class="col-lg-6 mx-auto">
+        <p class="lead mb-4">Portal KOKOLA Group is a gateway for all kokola apps.</p>
+        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center mt-5">
+          <a href="#" class="btn btn-primary btn-lg px-4 gap-3">E-HSD</a>
+          <a href="#" class="btn btn-outline-secondary btn-lg px-4">E-Recruitment</a>
+        </div>
+      </div>
+    </div>
+  </main>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+
+</html>
